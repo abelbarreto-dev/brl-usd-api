cf54c9eda027a5f9f702e95994456f8b25b34ff2
Abel Carvalho <64908368+abelbarreto-dev@users.noreply.github.com>
2023-08-17 11:13:15 -0300

    feat: create need models and enum data (#2)
    * feat: model money to access api backend
    
    * feat: quotation models to control the data from this api
    
    * feat: model type of swap
    
    * feat: quotation value created

---
+++ model/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+from decimal import Decimal
+
+from typing import Optional
+
+from enums.enums import SwapType
+
+
+class Money(BaseModel):
+    value_brl: Decimal = Decimal("0.00")
+    value_usd: Decimal = Decimal("0.00")
+    swap_type: Optional[SwapType] = None
+    quotation: Optional[Decimal] = None
+
+
+class QuotationUsdBrl(BaseModel):
+    value_in_brl: Decimal
+    usd_price: Decimal
+    value_usd: Decimal = Decimal("1.00")
+
+
+class QuotationBrlUsd(BaseModel):
+    value_in_usd: Decimal
+    brl_price: Decimal
+    value_brl: Decimal = Decimal("1.00")
