f2966fabb4aa6b34129d27efb651c5c62bd50a49
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-17 11:05:26 -0300

    feat: quotation models to control the data from this api

--- model/models.py
+++ model/models.py
@@ -6,3 +6,15 @@ from decimal import Decimal
 class Money(BaseModel):
     value_brl: Decimal = Decimal("0.00")
     value_usd: Decimal = Decimal("0.00")
+
+
+class QuotationUsdBrl(BaseModel):
+    value_in_brl: Decimal
+    usd_price: Decimal
+    value_usd: Decimal = Decimal("1.00")
+
+
+class QuotationBrlUsd(BaseModel):
+    value_in_usd: Decimal
+    brl_price: Decimal
+    value_brl: Decimal = Decimal("1.00")
