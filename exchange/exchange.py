d35f0868c359be816085886b8aeca0e23445384e
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-18 12:07:02 -0300

    feat: constructor and environment variables getter

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -1,2 +1,13 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+
 class Exchange:
-    pass
+    _usd_to_brl: str = None
+    _brl_to_usd: str = None
+
+    def __init__(self) -> None:
+        load_dotenv()
+        self._usd_to_brl = getenv("USD_TO_BRL")
+        self._brl_to_usd = getenv("BRL_TO_USD")
