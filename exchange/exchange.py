10d98859dae1112bce1c00a9dcaab66b616ba389
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-18 14:25:25 -0300

    chore: exchange extends from an abstract class with low level operations

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -1,21 +1,5 @@
-from os import getenv
-
-from dotenv import load_dotenv
+from investing.investing import Investing
 
 
-class Exchange:
-    _usd_to_brl: str = None
-    _brl_to_usd: str = None
-
-    def __init__(self) -> None:
-        load_dotenv()
-        self._usd_to_brl = getenv("USD_TO_BRL")
-        self._brl_to_usd = getenv("BRL_TO_USD")
-
-    @property
-    def usd_to_brl(self) -> str:
-        return self._usd_to_brl
-
-    @property
-    def brl_to_usd(self) -> str:
-        return self._brl_to_usd
+class Exchange(Investing):
+    pass
