77ca5b0fa7292442c97bac03fd3ef4e4a2ec1466
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-19 13:31:02 -0300

    test: brl to usd swap success

--- tests/routes/test_posts_routes.py
+++ tests/routes/test_posts_routes.py
@@ -17,6 +17,7 @@ from enums.enums import SwapType
 from tests.conftest import client
 
 URL_USD_BRL = "/usd-to-brl"
+URL_BRL_USD = "/brl-to-usd"
 CONTENT_TYPE = getenv("CONTENT_TYPE")
 
 
@@ -81,3 +82,24 @@ def test_usd_to_brl_failure_case_3(client: FlaskClient) -> None:
     )
 
     assert response.status_code == 400
+
+
+def test_brl_to_usd_success(client: FlaskClient) -> None:
+    brl_json = QuotationBrlUsd(
+        value_brl=Decimal("5.00")
+    )
+
+    response = client.post(
+        URL_BRL_USD,
+        data=brl_json.model_dump_json(),
+        content_type=CONTENT_TYPE,
+    )
+
+    assert response.status_code == 201
+
+    usd_obj = loads(response.data)
+
+    money = Money.model_validate(usd_obj)
+
+    assert money.value_brl == brl_json.value_brl
+    assert money.swap_type == SwapType.BRL_TO_USD
