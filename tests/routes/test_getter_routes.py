834204a419d16867dee24b145d8c8947a175cecd
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-19 01:06:06 -0300

    test: new test to get dollar quotation

---
+++ tests/routes/test_getter_routes.py
@@ -0,0 +1,27 @@
+from flask.testing import FlaskClient
+
+from decimal import Decimal
+
+from json import loads
+
+from models.models import Money
+
+from enums.enums import SwapType
+
+from tests.conftest import client
+
+
+def test_usd_to_brl_success(client: FlaskClient) -> None:
+    url_link = "/usd-price-brl"
+
+    response = client.get(url_link)
+
+    assert response.status_code == 201
+
+    data_obj = loads(response.data)
+
+    money = Money.model_validate(data_obj)
+
+    assert money.value_usd == Decimal("1.00")
+    assert money.swap_type == SwapType.USD_TO_BRL
+    assert money.value_brl == money.quotation
