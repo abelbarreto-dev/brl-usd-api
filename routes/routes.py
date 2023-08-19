0029c33a39775a2f3d065106b36512146baf77bf
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-19 13:18:50 -0300

    feat: route brl to usd linked to controller and json handler error

--- routes/routes.py
+++ routes/routes.py
@@ -18,11 +18,13 @@ app = create_app()
 
 
 @app.get("/usd-price-brl")
+@json_error_handler
 async def usd_price_brl() -> Response:
     return await Controller.usd_price_brl()
 
 
 @app.get("/brl-price-usd")
+@json_error_handler
 async def brl_price_usd() -> Response:
     return await Controller.brl_price_usd()
 
@@ -36,5 +38,8 @@ async def usd_to_brl():
 
 
 @app.post("/brl-to-usd")
-async def brl_to_usd(brl_json: QuotationBrlUsd) -> Response:
-    pass
+@json_error_handler
+async def brl_to_usd() -> Response:
+    brl_json = QuotationBrlUsd.model_validate(request.json)
+
+    return await Controller.brl_to_usd(brl_json)
